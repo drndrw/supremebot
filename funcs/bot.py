4fdbf874161a8dba4cb8f2a21afde9a5ca6deb4a
Andrew DaRe <andrew@duh.re>
2017-11-08 19:35:11 -0800

    Added size array to final output.

--- funcs/bot.py
+++ funcs/bot.py
@@ -11,20 +11,12 @@ class Find():
         self.parser = urllib.request.urlopen(parameters.parse_url).read()
         self.directory = bs.BeautifulSoup(self.parser,'lxml')
         self.searches = []
-        self.old_item = self.directory.find('article')
-        # while True:
-        #     self.new_item = self.directory.find('article')
-        #     if self.new_item == self.old_item:
-        #         print('NO UPDATES: ' + str(time.time()))
-        #         continue
-        #     else:
-        #         print('UPDATED: ' + str(time.time()))
-        #         break
 
         for link in self.directory.find_all('article'):
             #Start loop over if item is sold out on main page
             if link.find(class_="sold_out_tag"):
                 continue
+
             #Investigate if item is available
             else:
                 #Check if item is in a category of interest
@@ -36,36 +28,47 @@ class Find():
                             # print(parameters.base_url + link.a.get('href'))
                             self.item_parse = urllib.request.urlopen(parameters.base_url + link.a.get('href')).read()
                             self.item_page = bs.BeautifulSoup(self.item_parse,'lxml')
+
                             #Check if sold out (on page)
                             if self.item_page.find(attrs={'class':'sold-out'}):
                                 print(Colors.RED + "SOLD OUT" + Colors.END)
                                 continue
+
                             #Search category keywords in title
                             self.keywords = 0
                             if item[1]['keywords']:
                                 for keyword in item[1]['keywords']:
                                     if re.search(keyword, self.item_page.title.text, re.M|re.I):
                                         self.keywords += 1
+
                             #Search global keywords in title
                             #In the future, check for duplicate keywords in against category keywords, not important now
                             if clothing.global_keywords:
                                 for keyword in clothing.global_keywords:
                                     if re.search(keyword, self.item_page.title.text, re.M|re.I):
                                         self.keywords += 1
-                            #Locate the price, add to object
+
+                            #Locate the price
                             self.price = self.item_page.find(attrs={'data-currency':'USD'})
                             if self.price:
-                                pass
+                                self.price = int(self.price.text[1:])
                             else:
-                                print("Non USD")
+                                #Will worry about conversions later
                                 self.price = 0
+
                             #Find sizes
-                            self.sizes_select = self.item_page.find('select')
-                            print(self.sizes_select)
+                            self.sizes_select = self.item_page.find_all('option')
+                            if self.sizes_select:
+                                self.sizes = [size.text for size in self.sizes_select]
+                            else:
+                                self.sizes = None
+
                             #Terminal Display
                             print(Colors.BOLD + self.item_page.title.text[9:] + Colors.END)
                             print("  Category: " + item[0])
-                            print("  Price: " + self.price.text)
+                            print("  Price: " + str(self.price))
                             print("  Keywords: " + str(self.keywords))
+                            print("  Sizes: " + ', '.join(self.sizes) if self.sizes else 'N/A')
+
                             #Write to output array
-                            self.searches.append({'Category': item[0],'Name': self.item_page.title.text[9:],'Price': int(self.price.text[1:]),'URL': link.a.get('href'),'Keywords': int(self.keywords)})
+                            self.searches.append({'Category': item[0],'Name': self.item_page.title.text[9:],'Price': self.price if self.price else 0,'URL': link.a.get('href'),'Keywords': int(self.keywords), 'Sizes': self.sizes})
